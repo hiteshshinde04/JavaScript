e18feee88e63edb529b5d1214caeba228f1e19c4
Hitesh Shinde <130219369+hiteshshinde04@users.noreply.github.com>
2024-10-18 11:28:32 +0530

    Add files via upload

---
+++ else.js
@@ -0,0 +1,14 @@
+let color="red";
+
+if(color="red"){
+    console.log("Stop!");
+}
+else if(color="yellow"){
+    console.log("Slow down");
+}
+else if(color="green"){
+    console.log("Go");
+}
+else{
+    console.log("Traffic light is broken");   
+}
